16740dba533290372d683028bde305fdf97a159b
Rishav Bharti <rishavbharti21@gmail.com>
2020-03-13 21:58:38 +0530

    Updates

---
+++ precache-manifest.9459299ec7bf506c883571876ec2c4c5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa03c3103d453a909683975050d61a14",
+    "url": "/pics/index.html"
+  },
+  {
+    "revision": "93def969e5960c9b25c8",
+    "url": "/pics/static/css/main.ba623a93.chunk.css"
+  },
+  {
+    "revision": "a2f96f1b45bab8aa1c5e",
+    "url": "/pics/static/js/2.bf9a6dbf.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/pics/static/js/2.bf9a6dbf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "93def969e5960c9b25c8",
+    "url": "/pics/static/js/main.8a716654.chunk.js"
+  },
+  {
+    "revision": "2251b8dd8072a085af31",
+    "url": "/pics/static/js/runtime-main.302674cc.js"
+  }
+]);
